013731e55521682ce1dfb987ad40f8aa19c3c4ee
karimchoukri <krm.rif@gmail.com>
2016-01-13 19:59:13 +0100

    Modificar uploader.php
    Se ha metido el codigo php que habia dentro de una pagina html con los
    links hacia framework, css, javascript.

--- uploader.php
+++ uploader.php
@@ -1,16 +1,33 @@
-<?php 
-	/*Indica la ruta de la carpeta contenedor, en este caso se llamara uploads*/
-	$target_path = "uploads/";
-	$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
-	/*move_upload_file mueve un archivo, lo pasas con las variables , lo comprueba y lo sube sino da error. */
-	if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
-		echo "El archivo ". basename( $_FILES['uploadedfile']['name']). " ha sido subido ";
-		echo '<a href="formulario.php"><button type="button">Volver</button></a> <br>';
-		$file_name=$_FILES['uploadedfile']['name'];
-		echo "<img src='uploads/$file_name' width='70%' height='70%'>";
-	} else{
-		echo "Ha ocurrido un error, trate de nuevo!";
-	} 
-	/*estas dos lineas muestrar la imagen al final */
-	
-?>
+<html>
+	<head>
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link type="text/css" rel="stylesheet" href="materialize/css/materialize.css"  media="screen,projection"/>
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+		<title>Bisbal</title>
+	</head>
+	<!--
+	enctype="multipart/form-data": es necesario para subir archivos, crea la forma que permite explorar en su búsqueda en el equipo local.
+	-->
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col s12">
+					
+		<?php 
+			/*Indica la ruta de la carpeta contenedor, en este caso se llamara uploads*/
+			$target_path = "uploads/";
+			$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
+			/*move_upload_file mueve un archivo, lo pasas con las variables , lo comprueba y lo sube sino da error. */
+			if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+				echo '<h1 class="center-align z-depth-5">Galeria</h1> </div>';
+				echo " <p class='center-align'> El archivo <strong>". basename( $_FILES['uploadedfile']['name']). "</strong> ha sido subido </p>";
+				echo '<a  href="formulario.php"><button id="volver"class="btn" type="button">Volver</button></a>';
+				$file_name=$_FILES['uploadedfile']['name'];
+				echo "<div class='foto'><img src='uploads/$file_name' width='70%' height='70%'></div>";
+			} else {
+				echo "Ha ocurrido un error, trate de nuevo!";
+			} 
+			
+		?>
+	</body>
+</html>
