c53158af5640bdb4fbbfd84eb5d670d25a8385a6
karimchoukri <krm.rif@gmail.com>
2016-01-20 19:35:11 +0100

    Editar Pagina de Error
    se ha añadido un titulo, un enlace para volver, el mensaje en rojo y
    negrita.

--- uploader.php
+++ uploader.php
@@ -13,21 +13,25 @@
 			<div class="row">
 				<div class="col s12">
 					
-		<?php 
-			/*Indica la ruta de la carpeta contenedor, en este caso se llamara uploads*/
-			$target_path = "uploads/";
-			$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
-			/*move_upload_file mueve un archivo, lo pasas con las variables , lo comprueba y lo sube sino da error. */
-			if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
-				echo '<h1 class="center-align z-depth-5">Galeria</h1> </div>';
-				echo " <p class='center-align'> El archivo <strong>". basename( $_FILES['uploadedfile']['name']). "</strong> ha sido subido </p>";
-				echo '<a  href="formulario.php"><button id="volver"class="btn" type="button">Volver</button></a>';
-				$file_name=$_FILES['uploadedfile']['name'];
-				echo "<div class='foto'><img src='uploads/$file_name' width='70%' height='70%'></div>";
-			} else {
-				echo "Ha ocurrido un error, trate de nuevo!";
-			} 
-			
-		?>
+				<?php 
+					/*Indica la ruta de la carpeta contenedor, en este caso se llamara uploads*/
+					$target_path = "uploads/";
+					$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
+					/*move_upload_file mueve un archivo, lo pasas con las variables , lo comprueba y lo sube sino da error. */
+					if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+						echo '<h1 class="center-align z-depth-5">Galeria</h1> </div>';
+						echo " <p class='center-align'> El archivo <strong>". basename( $_FILES['uploadedfile']['name']). "</strong> ha sido subido </p>";
+						echo '<a  href="formulario.php"><button id="volver"class="btn" type="button">Volver</button></a>';
+						$file_name=$_FILES['uploadedfile']['name'];
+						echo "<div class='foto'><img src='uploads/$file_name' width='70%' height='70%'></div>";
+					} else {
+						echo '<h1 class="center-align z-depth-5">Galeria</h1> </div>';
+						echo "<strong> <p class='center-align' style='color: red'> Ha ocurrido un error, trate de nuevo!</p></strong> ";
+						echo '<a  href="formulario.php"><button id="volver"class="btn" type="button">Volver</button></a>';
+					} 
+					
+				?>
+			</div>
+		</div>
 	</body>
 </html>
