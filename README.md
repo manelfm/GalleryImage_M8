f12f865424070e1d5550284e6820f00a1ef2cbe2
memnoch2005 <asdqwe123>
2016-02-01 19:25:34 +0100

    Modificaciones previas a la instalación
    Realizadas las preparaciones finales para la instalación de la
    aplicación, por Jose, Karim y Manel.

--- README.md
+++ README.md
@@ -8,13 +8,13 @@ El usuario visualizará las imágenes en un carrousel, además de visualizar ind
 
 ##Carpetas y archivos
 
-.--- Carpeta /MATERIALIZE: contiene los archivos necesarios para utilizar un framework.
-.--- Carpeta /UPLOADS: será el almacén de imágenes.
-.--- Index.php: iniciará la aplicación mostrando la galería de imágenes.
+.--- Carpeta /materialize: contiene los archivos necesarios para utilizar un framework.
+.--- Carpeta /uploads: será el almacén de imágenes.
+.--- index.php: iniciará la aplicación mostrando la galería de imágenes.
 .--- gallery.js: incluye las funciones necesarias para la galería de imágenes.
 .--- mystyle.css: archivo que define el estilo y la apariencia de la aplicación.
 .--- uploader.php: conecta con el almacén comprobando los formatos de las fotos y los errores y realiza la subida de imágenes.
 
 ##Nota importante
 
-El usuario no podrá eliminar imágenes desde la aplicación, solo podrá eliminarlas dirigiéndose a la carpeta /UPLOADS y eliminándolas manualmente.
+El usuario no podrá eliminar imágenes desde la aplicación, solo podrá eliminarlas dirigiéndose a la carpeta /uploads y eliminándolas manualmente.
--- uploader.php
+++ uploader.php
@@ -21,7 +21,7 @@
 					if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 					&& $imageFileType != "gif" ) {
 					echo "<p style= 'color: red;text-align: center; font-size:30px'>Lo sentimos, debe de introducir archivos JPG, JPEG, PNG y GIF<p>.";
-					echo '<a  href="formulario.php"><button id="volver"class="btn" type="button">Volver</button></a>';
+					echo '<a  href="index.php"><button id="volver"class="btn" type="button">Volver</button></a>';
 					return;
 					} 
 					
@@ -30,13 +30,13 @@
 					if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
 						echo '<h1 class="center-align z-depth-5">Galeria</h1> </div>';
 						echo " <p class='center-align'> El archivo <strong>". basename( $_FILES['uploadedfile']['name']). "</strong> ha sido subido </p>";
-						echo '<a  href="formulario.php"><button id="volver"class="btn" type="button">Volver</button></a>';
+						echo '<a  href="index.php"><button id="volver"class="btn" type="button">Volver</button></a>';
 						$file_name=$_FILES['uploadedfile']['name'];
 						echo "<div class='foto'><img src='uploads/$file_name' width='70%' height='70%'></div>";
 					} else {
 						echo '<h1 class="center-align z-depth-5">Galeria</h1> </div>';
 						echo "<strong> <p class='center-align' style='color: red'> Ha ocurrido un error, trate de nuevo!</p></strong> ";
-						echo '<a  href="formulario.php"><button id="volver"class="btn" type="button">Volver</button></a>';
+						echo '<a  href="index.php"><button id="volver"class="btn" type="button">Volver</button></a>';
 					} 
 					
 				?>
